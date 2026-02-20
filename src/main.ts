ccabf32de0f4debdcebe114ded41aecbd17df37a
Rayan-Walnut <gith945@gmail.com>
2026-02-20 20:42:30 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+function createWindow(): void {
+  const iconPath = path.join(app.getAppPath(), 'public', 'favicon.ico');
+  const isWindows = process.platform === 'win32';
+
+  const win = new BrowserWindow({
+    width: 900,
+    height: 600,
+    backgroundColor: '#0D0D0D',
+    titleBarStyle: 'hidden',
+    ...(isWindows
+      ? {
+          titleBarOverlay: {
+            color: '#0D0D0D',
+            symbolColor: '#FFFFFF',
+            height: 34,
+          },
+        }
+      : {}),
+    icon: iconPath,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+    },
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
